14ad6ece83077d06f364b9a83e5fcc73c2efcbb5
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-03-24 19:07:54 +0300

    Update hm2.js
    Bugs fixed. Redefined console.log

--- scripts/hm2.js
+++ scripts/hm2.js
@@ -6,6 +6,54 @@
 // length: выводит кол-во элементов в Стеке
 // clear: очищает Стек
 
+_log = console.log.bind(console);
+_warn = console.warn.bind(console);
+_err = console.error.bind(console);
+
+console.log = function() {
+    let logArguments = [];
+
+    logArguments.push(logTime());
+    for (let key in arguments) {
+        logArguments.push(arguments[key]);
+    }
+
+    _log.apply(this, logArguments);
+};
+
+console.warn = function() {
+    let logArguments = [];
+
+    logArguments.push(logTime());
+    for (let key in arguments) {
+        logArguments.push(arguments[key]);
+    }
+
+    _warn.apply(this, logArguments);
+};
+
+console.error = function() {
+    let logArguments = [];
+
+    logArguments.push(logTime());
+    for (let key in arguments) {
+        logArguments.push(arguments[key]);
+    }
+
+    _err.apply(this, logArguments);
+};
+
+function logTime() {
+    let date = new Date();
+    let options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+
+    return(date.toLocaleString("ru", options));
+}
+
 class Stack {
     constructor() {
         this.items = [];
@@ -23,11 +71,12 @@ class Stack {
         if (this.count == 0) return undefined;
         let delItem = this.items[this.count - 1];
         this.count--;
-        console.log(`Элемент ${delItem} удален`);
+        console.warn(`Элемент ${delItem} удален`);
         return delItem;
     }
 
     peek() {
+        if (this.count == 0) return undefined;
         console.log(`Верхний элемент '${this.items[this.count - 1]}', под номером ${this.count - 1}`);
         return this.count - 1;
     }
@@ -40,7 +89,7 @@ class Stack {
     clear() {
         this.items = [];
         this.count = 0;
-        console.log('Стек очищен...');
+        console.warn('Стек очищен...');
         return this.items;
     }
 }
@@ -48,18 +97,19 @@ class Stack {
 const stack = new Stack();
 
 window.onload = function () {
-    const main =  document.querySelector("#mainContainer");
-    const addB = document.querySelector("#add");
-    const delB = document.querySelector("#del");
-    const clearB = document.querySelector("#clear");
+    const addButton = document.querySelector("#add");
+    const delButton = document.querySelector("#del");
+    const clearButton = document.querySelector("#clear");
     const divCount = document.querySelector("#count");
 
-    addB.onclick = function () {
+    addButton.onclick = function () {
         let container = document.getElementById("container");
         let text = document.getElementById('text');
 
         if (text.value.trim()) {
             let mas = text.value.split(/,\s*/);
+
+            const TIME_START = new Date;
 
             mas.forEach(function(entry) {
                 let stackBlock = document.createElement("div");
@@ -71,26 +121,30 @@ window.onload = function () {
 
                 stLength();
             });
+
+           const TIME_END = new Date,
+               CHRONOMETRY = TIME_END - TIME_START;
+
+            console.log('Операция добавления заняла -', CHRONOMETRY, 'мс');
         }
     };
 
-    delB.onclick = function () {
-        let div = document.getElementById(`sb${stack.peek()}`);
-        div.remove();
+    delButton.onclick = function () {
+        try {
+            let div = document.getElementById(`sb${stack.peek()}`);
+            div.remove();
 
-        stack.pop();
+            stack.pop();
 
-        stLength();
+            stLength();
+        } catch (err) {
+            console.warn('Стек пуст...');
+        }
     };
 
-    clearB.onclick = function () {
+    clearButton.onclick = function () {
         let container = document.getElementById("container");
-        container.remove();
-
-        let div = document.createElement("div");
-        div.className = "stack";
-        div.id = 'container';
-        main.append(div);
+        container.innerHTML = '';
 
         stack.clear();
 
